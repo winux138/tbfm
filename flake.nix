24af20816e65cd27a7665f36bc62d9dfcc8cc310
Julien THOMAS <thomas.julien@protonmail.com>
2025-09-06 15:02:08 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "{{NAME}}";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            nativeBuildInputs = [
+              clang_21
+              cmake
+              ninja
+            ];
+
+            shellHook = '''';
+          };
+      }
+    );
+}
