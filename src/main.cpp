81a9b6fcf0dae4579a1589a1d12daa45dfb7fb71
Julien THOMAS <thomas.julien@protonmail.com>
2025-09-07 15:20:43 +0200

    Add boilerplate for ncurses with an example

--- src/main.cpp
+++ src/main.cpp
@@ -84,12 +84,38 @@ main (int argc, char **argv)
     (void)argc;
     (void)argv;
 
-    std::cout << "Welcome to TBFM!" << std::endl;
+    int ch{ 0 };
+    initscr ();            /* Start curses mode       */
+    raw ();                /* Line buffering disabled */
+    keypad (stdscr, TRUE); /* We get F1, F2 etc..    */
+    noecho ();             /* Don't echo() while we do getch */
 
-    bool should_exit{ false };
-    while (!should_exit) {
-
-        print_directory ();
-        should_exit = prompt_user_action ();
+    printw ("Type any character to see it in bold\n");
+    ch = getch ();                 /* If raw() hadn't been called
+                                    * we have to press enter before it
+                                    * gets to the program     */
+    if (ch == KEY_F (1))           /* Without keypad enabled this will */
+        printw ("F1 Key pressed"); /*  not get to us either  */
+    /* Without noecho() some ugly escape charachters might have been printed on
+     * screen*/
+    else {
+        printw ("The pressed key is ");
+        attron (A_BOLD);
+        printw ("%c", ch);
+        attroff (A_BOLD);
     }
+    refresh (); /* Print it on to the real screen */
+    getch ();   /* Wait for user input */
+    endwin ();  /* End curses mode      */
+
+    // std::cout << "Welcome to TBFM!" << std::endl;
+
+    // bool should_exit{ false };
+    // while (!should_exit) {
+
+    //     print_directory ();
+    //     should_exit = prompt_user_action ();
+    // }
+
+    return 0;
 }
