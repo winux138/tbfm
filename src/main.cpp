ba8b652be7fbde7fdf66ef432b2ebc2b3adc085f
Julien THOMAS <thomas.julien@protonmail.com>
2025-09-25 16:03:27 +0200

    Refactor into a class

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,100 @@
+#include <algorithm>
 #include <cassert>
 #include <filesystem>
 #include <iostream>
 #include <ranges>
-#include <string>
+// #include <string>
 #include <vector>
 
 #include <ncurses.h>
 
-int cursor{ 2 };
-auto path{ std::filesystem::current_path () };
-std::vector<std::filesystem::path> sub_directories{};
-
-std::vector<std::filesystem::path>
-get_subdirectories (void)
+class FolderView
 {
-    std::vector<std::filesystem::path> subdirectories{};
-    for (const auto &file : std::filesystem::directory_iterator (path))
-        if (file.is_directory ())
-            subdirectories.push_back (file.path ());
-    return subdirectories;
-}
+  private:
+    int m_cursor{};
+    std::filesystem::path m_path{};
+    std::vector<std::filesystem::path> m_path_subdirs{};
+    std::vector<std::filesystem::path> m_path_files{};
+    // TODO ncurses stuff
 
-std::vector<std::filesystem::path>
-get_files (void)
-{
-    std::vector<std::filesystem::path> files{};
-    for (const auto &file : std::filesystem::directory_iterator (path))
-        if (file.is_regular_file ())
-            files.push_back (file.path ());
-    return files;
-}
+    void
+    print_direntry (const char *entry_name, const int row)
+    {
+        std::cout << "row: " << row << "cursor" << m_cursor << std::endl;
+        if (row == m_cursor)
+            attron (A_REVERSE);
+        mvprintw (row, 2, "%s", entry_name);
+        if (row == m_cursor)
+            attroff (A_REVERSE);
+    }
 
-void
-print_direntry (const char *entry_name, const int row)
-{
-    if (row == cursor)
-        attron (A_REVERSE);
-    mvprintw (row, 2, "%s", entry_name);
-    if (row == cursor)
-        attroff (A_REVERSE);
-}
+  public:
+    FolderView ()
+        : m_cursor{ 2 }, m_path{ std::filesystem::current_path () },
+          m_path_subdirs{}, m_path_files{}
+    {
+        m_path_subdirs
+            = std::filesystem::directory_iterator (m_path)
+              | std::views::filter ([] (auto e) { return e.is_directory (); })
+              | std::views::transform ([] (auto e) { return e.path (); })
+              | std::ranges::to<std::vector> ();
+        m_path_files
+            = std::filesystem::directory_iterator (m_path)
+              | std::views::filter (
+                  [] (auto e) { return e.is_regular_file (); })
+              | std::views::transform ([] (auto e) { return e.path (); })
+              | std::ranges::to<std::vector> ();
+    }
+
+    void
+    move_up (void)
+    {
+        --m_cursor;
+        if (m_cursor < 2)
+            m_cursor = 2;
+    }
+
+    void
+    move_down (void)
+    {
+        ++m_cursor;
+        // TODO: Do something about warning
+        if (m_cursor > 2 + m_path_subdirs.size () + m_path_files.size ())
+            m_cursor = 2 + m_path_subdirs.size () + m_path_files.size ();
+    }
+
+    void
+    display ()
+    {
+        clear ();
+        printw ("Welcome to TBFM!\n");
+        int row{ 2 };
+        printw ("%s:\n", m_path.c_str ());
+
+        attron (COLOR_PAIR (1));
+        print_direntry ("..", row++);
+        for (const auto &subdirectory :
+             std::filesystem::directory_iterator (m_path)
+                 | std::views::filter (
+                     [] (const auto &e) { return e.is_directory (); })
+                 | std::views::transform (
+                     [] (const auto &e) { return e.path (); }))
+            print_direntry (subdirectory.filename ().c_str (), row++);
+        attroff (COLOR_PAIR (1));
+
+        for (const auto &file :
+             std::filesystem::directory_iterator (m_path)
+                 | std::views::filter (
+                     [] (const auto &e) { return e.is_regular_file (); })
+                 | std::views::transform (
+                     [] (const auto &e) { return e.path (); }))
+            print_direntry (file.filename ().c_str (), row++);
+
+        // TODO: Footer bar with keymaps
+
+        refresh ();
+    }
+};
 
 int
 main (int argc, char **argv)
@@ -59,32 +114,17 @@ main (int argc, char **argv)
 
     curs_set (0);
 
+    FolderView folder_view{};
     bool should_exit{ false };
     while (!should_exit) {
-        clear ();
-        printw ("Welcome to TBFM!\n");
+        folder_view.display ();
 
-        // TODO: Footer bar with keymaps
-
-        int row{ 2 };
-        printw ("%s:\n", path.c_str ());
-
-        attron (COLOR_PAIR (1));
-        print_direntry ("..", row++);
-        for (const auto &subdirectory : get_subdirectories ())
-            print_direntry (subdirectory.filename ().c_str (), row++);
-        attroff (COLOR_PAIR (1));
-
-        for (const auto &file : get_files ())
-            print_direntry (file.filename ().c_str (), row++);
-
-        refresh ();
         switch (getch ()) {
         case 'k':
-            --cursor;
+            folder_view.move_up ();
             break;
         case 'j':
-            ++cursor;
+            folder_view.move_down ();
             break;
         case 'q':
             should_exit = true;
